ff6a8187444023db0ef055a4fef7451c9429a3c8
imjog <imjog24@gmail.com>
2018-04-08 17:03:59 +0530

    Added carthage support

---
+++ Example/AnimatedMaskLabel/AnimatedMaskLabel.h
@@ -0,0 +1,19 @@
+//
+//  AnimatedMaskLabel.h
+//  AnimatedMaskLabel
+//
+//  Created by JOGENDRA on 08/04/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnimatedMaskLabel.
+FOUNDATION_EXPORT double AnimatedMaskLabelVersionNumber;
+
+//! Project version string for AnimatedMaskLabel.
+FOUNDATION_EXPORT const unsigned char AnimatedMaskLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnimatedMaskLabel/PublicHeader.h>
+
+
